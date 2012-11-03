eadf30ae8da1d4c39b1861328f956cbc88205558
Marcus Morris <10mmorris1194@gmail.com>
2012-11-02 21:05:47 -0400

    Edited

--- treeMap.py
+++ treeMap.py
@@ -19,16 +19,27 @@ def mkNonEmptyMap(left, key, val, right):
     return aMap
 
 def mapInsert(key, val, aMap):
-    while isinstance(aMap, EmptyMap) is False:
-        if aMap.key == key:
-            aMap.val = val
-            break
-        elif aMap.key < key:
-            aMap = aMap.left
-        elif aMap.key > key:
-            aMap = aMap.right
-
     if isinstance(aMap, EmptyMap):
+        aMap = mkNonEmptyMap(mkEmptyMap(),key,val,mkEmptyMap())
+    elif aMap.key == key:
+        aMap.val = val
+    elif aMap.key < key:
+        aMap = mapInsert(key, val, aMap.left)
+    elif aMap.key > key:
+        aMap = mapInsert(key, val, aMap.right)
+    elif isinstance(aMap, EmptyMap):
         aMap = mkNonEmptyMap(mkEmptyMap(),key,val,mkEmptyMap())
 
     return aMap
+
+def main():
+	aMap = mkNonEmptyMap(mkNonEmptyMap(mkEmptyMap(),'eight',8,mkEmptyMap()),'five',5,mkNonEmptyMap(mkEmptyMap(),'four',4,mkEmptyMap()))
+	print(aMap.val)
+	if isinstance(aMap.left, EmptyMap) is False:
+		print(aMap.left.val)
+	if isinstance(aMap.right,EmptyMap) is False:
+		print(aMap.right.val)
+
+main()
+
+
